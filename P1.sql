b46c89887ee02d8e1b6191ae3f0d2caab318dd81
AbooaliRosha <121517867+AbooaliRosha@users.noreply.github.com>
2023-05-17 19:07:27 -0400

    P1.sql

--- P1.sql
+++ P1.sql
@@ -13,3 +13,9 @@ Query the NAME field for all American cities in the CITY table with populations 
 Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
 */
 SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
+/*
+PART C)
+Query all columns (attributes) for every row in the CITY table.
+*/
+Query all columns (attributes) for every row in the CITY table.
