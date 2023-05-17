ae28ab7ac8346b2b3e60a5d84f083b2d4f25b21b
AbooaliRosha <121517867+AbooaliRosha@users.noreply.github.com>
2023-05-17 18:55:35 -0400

    P1.sql

--- P1.sql
+++ P1.sql
@@ -1,7 +1,15 @@
 /*
+PART A)
 Query all columns for all American cities in the CITY table with populations larger than 100000. 
 The CountryCode for America is USA.
 Link: https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
 */
 SELECT * FROM CITY 
 WHERE POPULATION >= 100000 && COUNTRYCODE = "USA";
+
+/*
+PART B)
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+*/
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
