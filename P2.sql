7b941dcde0624e246b2cca25863730cb6d607db5
AbooaliRosha <121517867+AbooaliRosha@users.noreply.github.com>
2023-05-26 17:08:26 -0400

    P2.sql

--- P2.sql
+++ P2.sql
@@ -6,3 +6,10 @@ as both their first and last characters. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION 
 WHERE (CITY LIKE 'a%' OR CITY LIKE 'e%' OR CITY LIKE 'i%' OR CITY LIKE 'o%' OR CITY LIKE 'u%') 
 AND (CITY LIKE '%a' OR CITY LIKE '%e'OR CITY LIKE '%i'OR CITY LIKE '%o' OR CITY LIKE '%u');
+
+/*
+PART B)
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+*/
+SElECT DISTINCT CITY FROM STATION 
+WHERE (CITY NOT LIKE 'a%' AND CITY NOT LIKE 'e%' AND CITY NOT LIKE 'i%' AND CITY NOT LIKE 'o%' AND CITY NOT LIKE 'u%')
